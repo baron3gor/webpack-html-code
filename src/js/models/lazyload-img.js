7e4450f6dc7aef948ca84deb28aac501237f4d55
Baron Egor <b3gor@yandex.ru>
2021-02-11 23:13:27 +0200

    1102 final

--- src/js/models/lazyload-img.js
+++ src/js/models/lazyload-img.js
@@ -1,16 +1,16 @@
 if ("loading" in HTMLImageElement.prototype) {
-	let images = document.querySelectorAll('img[loading="lazy"]');
-	let sources = document.querySelectorAll("source[data-srcset]");
-	sources.forEach(function (source) {
-		source.srcset = source.dataset.srcset;
-	});
-	images.forEach(function (img) {
-		img.src = img.dataset.src;
-	});
+   let images = document.querySelectorAll('img[loading="lazy"]');
+   let sources = document.querySelectorAll("source[data-srcset]");
+   sources.forEach(function (source) {
+      source.srcset = source.dataset.srcset;
+   });
+   images.forEach(function (img) {
+      img.src = img.dataset.src;
+   });
 } else {
-	//if no support, async load the lazysizes plugin
-	let script = document.createElement("script");
-	script.async = true;
-	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
-	document.body.appendChild(script);
+   //if no support, async load the lazysizes plugin
+   let script = document.createElement("script");
+   script.async = true;
+   script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+   document.body.appendChild(script);
 }
--- src/js/models/sticky-nav.js
+++ src/js/models/sticky-nav.js
@@ -1,9 +1,9 @@
-$(window).on('scroll', function() {
-    if($(document).scrollTop() > 40) {
-        $('.pxr-sticky-top-line').addClass('active');
-        $('.pxr-side-mobile').removeClass('pxr-side-menu-open');
-    } else {
-        $('.pxr-sticky-top-line').removeClass('active');   
-        $('.pxr-side-mobile').removeClass('pxr-side-menu-open');
-    }
+$(window).on('scroll', function () {
+   if ($(document).scrollTop() > 40) {
+      $('.pxr-sticky-top-line').addClass('active');
+      $('.pxr-side-mobile').removeClass('pxr-side-menu-open');
+   } else {
+      $('.pxr-sticky-top-line').removeClass('active');
+      $('.pxr-side-mobile').removeClass('pxr-side-menu-open');
+   }
 });
